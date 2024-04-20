7127c32635b4f9ad07269a4f2bf8f6afe582d3be
wenyamu <142462665+wenyamu@users.noreply.github.com>
2024-04-20 15:14:22 +0800

    Update hexo-entrypoint.sh

--- hexo/hexo-entrypoint.sh
+++ hexo/hexo-entrypoint.sh
@@ -10,7 +10,8 @@ if [ -z "$(ls)" ]; then
       hexo-renderer-pug \
       hexo-generator-search \
       hexo-deployer-rsync \
-      hexo-deployer-git
+      hexo-deployer-git \
+      hexo-generator-sitemap
 fi
 
 # 启动 hexo
